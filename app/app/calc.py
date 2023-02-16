e55570e271303ba21d80f0d4c7261ebe1ad031a6
Abhi <abhishek802314@gmail.com>
2023-02-16 19:15:06 +0530

    Added Database.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,5 +2,7 @@
 def add(x, y):
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     return y - x
+
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,9 @@ from . import calc
 class CalcTests(SimpleTestCase):
     
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
 
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
